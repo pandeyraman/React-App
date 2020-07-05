a4440907864687b7427a2cd147195ceb8226845c
pandeyraman <ramanpandey19@gmail.com>
2020-07-05 08:55:31 +0545

    Redux Added

--- src/components/RenderLeaderComponent.js
+++ src/components/RenderLeaderComponent.js
@@ -1,13 +1,5 @@
 import React from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Card,
-  CardBody,
-  CardHeader,
-  Media,
-} from "reactstrap";
-import { Link } from "react-router-dom";
+import { Media } from "reactstrap";
 const RenderLeader = ({ leaders }) => {
   if (leaders != null) {
     console.log(leaders, "leaders");
---
+++ src/redux/reducer.js
@@ -0,0 +1,19 @@
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+
+import { LEADERS } from "../shared/leaders";
+
+import { COMMENTS } from "../shared/comments";
+
+export const initialState = {
+  dishes: DISHES,
+  comments: COMMENTS,
+  promotions: PROMOTIONS,
+  leaders: LEADERS,
+};
+
+const Reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default Reducer;
